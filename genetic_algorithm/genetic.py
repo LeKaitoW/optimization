73ee9e777ec62943d1b9fd2487931960a703d8bd
Olga Zudina <le.chat.noir.kaito@gmail.com>
2017-03-17 17:02:20 +0300

    fix algoritm
    add some weird code

--- genetic_algorithm/genetic.py
+++ genetic_algorithm/genetic.py
@@ -3,10 +3,14 @@ from agent import Agent
 
 N = 2 #2, 4, 8, 16, 32
 population_size = 20
-precision = 2
+precision = 4
 max_counter = 1000
+function = "sphere"
+
 population = []
 intermediate_population = []
+#random.seed(234547)
+#random.seed(9996)
 
 def init_population(population_size, max, precision):
 	for index in range(population_size):
@@ -21,7 +25,7 @@ def selection(population):
 	parents = []
 	parents.append(population.pop(random.randrange(len(population))))
 	max_distance = 0
-	#second_parent=0 костыль
+	#second_parent=0
 	for j, agent in enumerate(population):
 		distance = 0
 		for index, x in enumerate(agent.values):
@@ -30,8 +34,9 @@ def selection(population):
 		if distance>max_distance:
 			max_distance = distance
 			second_parent = j
+		#else:
+		#	print(0)
 	parents.append(population.pop(second_parent))
-	intermediate_population.extend(parents)
 	return parents
 
 
@@ -43,8 +48,16 @@ def arithmetical_crossing(parents):
 	for i in range(N):
 		x_first.append(round(u_first*parents[0].values[i]+(1-u_first)*parents[1].values[i], precision))
 		x_second.append(round(u_second*parents[1].values[i]+(1-u_second)*parents[0].values[i], precision))
-	intermediate_population.append(Agent(N, x_first))
-	intermediate_population.append(Agent(N, x_second))
+	new_first_agent = Agent(N, x_first)
+	new_second_agent = Agent(N, x_second)
+	rates = {}
+	for parent in parents:
+		rates.update({parent.rate : parent})
+	rates.update({new_first_agent.rate : new_first_agent})
+	rates.update({new_second_agent.rate : new_second_agent})
+	keys = sorted(rates.keys())
+	intermediate_population.append(rates.get(keys[0]))
+	intermediate_population.append(rates.get(keys[1]))
 
 
 def mutation_Michalewicz(agent, counter):
@@ -52,28 +65,15 @@ def mutation_Michalewicz(agent, counter):
 	seq = [1, -1]
 	u_first = random.choice(seq)
 	r = random.random()
-	delta = 1*(1-pow(r, (pow(1-(counter/max_counter),b)))) #интервал |[x-;x+]|=2
+	delta = 0.1*(1-pow(r, (pow(1-(counter/max_counter),b)))) #интервал |[x-;x+]|=2
 	x_new = []
 	for value in agent.values:
 		x_new.append(round(value+u_first*delta, precision))
-	intermediate_population.append(Agent(N, x_new))
-
-
-def test_selection(intermediate_population, population):
-		average = []
-		for agent in intermediate_population:
-			average.append(agent.rate)
-		print(average)
-		average = sorted(average)[:population_size]
-		#random.shuffle(average)
-		#del average[9:]
-		print(average)
-		for agent in intermediate_population:
-			if agent.rate in average:
-				population.append(agent)
-			if len(population)>population_size-1:
-				break
-		del intermediate_population[:]
+	new_agent = Agent(N, x_new)
+	if new_agent.rate<agent.rate:
+		intermediate_population.append(new_agent)
+	else:
+		intermediate_population.append(agent)
 
 
 def can_stop(population):
@@ -83,26 +83,30 @@ def can_stop(population):
 			return True
 	return False
 
-
-def encoding(population):
-	encoding_population = []
-	for value in population:
-		encoding_population.append((u'%.2f'%value).replace('.', ''))
-	print(encoding_population)		
 		
-population = init_population(population_size, 100, precision)
+def find_best(population):
+	rates = []
+	for agent in population:
+		rates.append(agent.rate)
+	print('best=', min(rates))
+
+
+population = init_population(population_size, 10, precision)
 
 counter = 1
 
 while ((not can_stop(population)) and (counter < max_counter)):
 #while counter < 10:
-	print(counter)
-	for i in range(population_size//3):
+	print('it = ', counter)
+	for i in range(population_size//20):
 		arithmetical_crossing(selection(population))
-	for agent in population:
-		mutation_Michalewicz(agent, counter)
-		population.remove(agent)
-	test_selection(intermediate_population,population)
+	for j in range(len(population)):
+		mutation_Michalewicz(population[j], counter)
+	population = intermediate_population[:]
+	find_best(population)
+	del intermediate_population[:]
 	counter+=1
+	for agent in population:
+		print(agent.rate)
 
 print(len(population))
